c39866f4d65fc14cf917d80af76bae2d8a997d31
charles <charleshparmley@icloud.com>
2021-10-28 02:11:07 -0500

    gitignore

--- ESP32/stepper_controller.py
+++ ESP32/stepper_controller.py
@@ -21,16 +21,13 @@ def send_position(position,screen_width,screen_height):
          step_setting = '16'
       elif distance_x > 1:
          step_setting = '32'
-      move_string.append(step_setting)
-   
-      # Check if the target is within the margin of error
-      if distance_x > 1:
-         # If target right of center
-         if position[0] - screen_width/2 > 0:
-            move_string.append('r')
-         # If target left of center
-         if position[0] - screen_width/2 < 0:
-            move_string.append('l')
+
+      # If target right of center
+      if position[0] - screen_width/2 > 0:
+         move_string.append('r')
+      # If target left of center
+      if position[0] - screen_width/2 < 0:
+         move_string.append('l')
 
    distance_y = abs(int(position[1] - screen_height/2))
    # for the distance from center of target to center of camera on y axis
@@ -49,17 +46,15 @@ def send_position(position,screen_width,screen_height):
          step_setting = '32'
       move_string.append(step_setting)
 
-      # If not withing our margin of error
-      if distance_y > 1:
-         # If target below center
-         if position[1] - screen_height/2 > 0:
-            move_string.append('d')
-         # If target above center
-         if position[1] - screen_height/2 < 0:
-            move_string.append('u')
+      # If target below center
+      if position[1] - screen_height/2 > 0:
+         move_string.append('d')
+      # If target above center
+      if position[1] - screen_height/2 < 0:
+         move_string.append('u')
 
 
-   if abs(position[0] - screen_width/2) < 35 and abs(position[1] - screen_height/2) < 35:
+   if distance_x < 35 and distance_y < 35:
       print('Locked..', position)
    write_moves(move_string)
 
