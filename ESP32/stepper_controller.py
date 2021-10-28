7dee2de53c6fc3e96c737badfee34bf2badbd0ec
charles <charleshparmley@icloud.com>
2021-10-28 09:44:40 -0500

    gitignore

--- ESP32/stepper_controller.py
+++ ESP32/stepper_controller.py
@@ -8,13 +8,13 @@ def send_position(position,screen_width,screen_height):
    buffer = 5
    # for the distance from center of target to center of camera on x axis
    distance_x = abs(int(position[0] - screen_width/2))
-   if distance_x > error_margin:
-      # if distance_x > 96:
-      #    step_setting = '1'
-      # elif distance_x > 64:
-      #    step_setting = '2'
-      # elif distance_x > 32:
-      #    step_setting = '4'
+   for move in range(distance_x):
+      if distance_x > 96:
+         step_setting = '1'
+      elif distance_x > 64:
+         step_setting = '2'
+      elif distance_x > 32:
+         step_setting = '4'
       if distance_x > 24:
          step_setting = '8'
       elif distance_x > 8:
@@ -26,22 +26,24 @@ def send_position(position,screen_width,screen_height):
       # If target right of center
       if position[0] - screen_width/2 > 0:
          move_string.append('r')
+         distance_x -= 32/int(step_setting)
       # If target left of center
       if position[0] - screen_width/2 < 0:
          move_string.append('l')
+         distance_x -= 32/int(step_setting)
 
    distance_y = abs(int(position[1] - screen_height/2))
    # for the distance from center of target to center of camera on y axis
-   if distance_y > error_margin:
-      # if distance_y > 96:
-      #    step_setting = '1'
-      # elif distance_y > 64:
-      #    step_setting = '2'
-      # elif distance_y > 32:
-      #    step_setting = '4'
-      if distance_y > 24:
-         step_setting = '8'
+   for move in range(distance_y):
+      if distance_y > 32:
+         step_setting = '1'
+      elif distance_y > 16:
+         step_setting = '2'
       elif distance_y > 8:
+         step_setting = '4'
+      if distance_y > 4:
+         step_setting = '8'
+      elif distance_y > 2:
          step_setting = '16'
       elif distance_y > 1:
          step_setting = '32'
@@ -50,9 +52,11 @@ def send_position(position,screen_width,screen_height):
       # If target below center
       if position[1] - screen_height/2 > 0:
          move_string.append('d')
+         distance_y -= 32/int(step_setting)
       # If target above center
       if position[1] - screen_height/2 < 0:
          move_string.append('u')
+         distance_y -= 32/int(step_setting)
 
 
    if distance_x < 35 and distance_y < 35:
